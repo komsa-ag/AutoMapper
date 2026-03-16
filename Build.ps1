50c9f0670a70b930d0253c48a4b5e7aae2046d92
Jan Friedrich <jan.friedrich@komsa.de>
2026-03-16 09:52:46 +0100

    disable tests

--- Build.ps1
+++ Build.ps1
@@ -26,6 +26,6 @@ $artifacts = ".\artifacts"
 
 if(Test-Path $artifacts) { Remove-Item $artifacts -Force -Recurse }
 
-exec { & dotnet test -c Release --results-directory $artifacts -l trx }
+exec { & dotnet build .\src\AutoMapper\AutoMapper.csproj -c Release }
 
 exec { & dotnet pack .\src\AutoMapper\AutoMapper.csproj -c Release -o $artifacts --no-build }
